f04706aec4b1cbbad5781306939d0a5637804e19
MizuhoObama <Mizuho.Obama@ibm.com>
2024-07-31 06:47:55 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,3 +1,12 @@
-// Create new server
-// Run the server
-// Add the following code to the comments.js file
+// Create web server
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var mysql = require('mysql');
+var fs = require('fs');
+var path = require('path');
+var db = require('./db.js');
+var connection = db.connect();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
